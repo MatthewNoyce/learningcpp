d2e81853568e5885afe566c239f3ec56b79e1107
MatthewNoyce <matthewnoyce@icloud.com>
2024-10-09 11:46:20 +0100

    tried to get fib.cpp to produce larger fibbonaci numbers but it isnt currently

--- fibbonaci/fib.cpp
+++ fibbonaci/fib.cpp
@@ -7,7 +7,7 @@ void fib(int iter, int start1=0, int start2=1){
     }
     else{
         std::cout << start1 << ", ";
-        int newnum = start1 + start2;
+        unsigned long long int newnum = start1 + start2;
         start1 = start2;
         start2 = newnum;
         fib(iter-1, start1, start2);
--- fibbonaci/main.cpp
+++ fibbonaci/main.cpp
@@ -2,7 +2,20 @@
 
 //This could have all been done within one program but I wanted to practice using headers 
 //its also cool to do recursion
+
+
 int main(){
+    /*
+    fib(int iter, int start1=0, int start2=0)
+    prints out iter many fibbonaci numbers starting with start1 and start2 
+    */
     fib(20);
     fib(20, 2, 3);
+    //500 doesnt work with int
+    //going to try long
+    //long long int doesnt seem to work either 
+    //same for unsigned long long int
+    //unsigned long long int should work up to 8bytes and this is not going that high.
+    //something might be faulty with the logic but it seems to get the first lot okay 
+    fib(300);
 }
